e39e17a064b6287905dc2b1f428a6a08e5c51c2e
Glitch (god-playlist1) <none>
2018-02-09 12:00:47 +0000

    ⚾️🛥 Checkpoint views/partials/listsongs.hbs:653028/18 controllers/dashboard.js:653028/26

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -7,7 +7,25 @@ const dashboard = {
     logger.info('dashboard rendering');
      const viewData = {
       title: 'Playlist Dashboard',
+       const sonatas = {
+  title: 'Sonatas',
+  songs: [
+    {
+      title: 'Piano Sonata No. 3',
+      artist: 'Beethoven',
+    },
+    {
+      title: 'Piano Sonata No. 7',
+      artist: 'Beethoven',
+    },
+    {
+      title: 'Piano Sonata No. 10',
+      artist: 'Beethoven',
+    },
+  ],
+};
     };
+
     response.render('dashboard', viewData);
   },
 };
