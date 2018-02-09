6f5c89a96c55db7a043f82c54cf438b0ef91aa67
Glitch (god-playlist1) <none>
2018-02-09 12:11:04 +0000

    🍥🍭 Checkpoint controllers/dashboard.js:653028/103 README.md:653028/35 models/playlist-store.js:653028/3

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,31 +1,16 @@
 'use strict';
 
 const logger = require('../utils/logger');
+const sonatas = require('../models/playlist-store.js');
 
 const dashboard = {
   index(request, response) {
     logger.info('dashboard rendering');
-     const viewData = {
+    const viewData = {
       title: 'Playlist Dashboard',
-       const sonatas = {
-  title: 'Sonatas',
-  songs: [
-    {
-      title: 'Piano Sonata No. 3',
-      artist: 'Beethoven',
-    },
-    {
-      title: 'Piano Sonata No. 7',
-      artist: 'Beethoven',
-    },
-    {
-      title: 'Piano Sonata No. 10',
-      artist: 'Beethoven',
-    },
-  ],
-};
+      playlist: sonatas,
     };
-
+    logger.info('about to render', sonatas);
     response.render('dashboard', viewData);
   },
 };
